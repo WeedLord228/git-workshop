dacd28a0cd73979a04fc147764509c98d2658c28
WeedLord228 <zajtzevar@gmail.com>
2019-10-05 16:07:10 +0500

    decommented code (gay)

--- musicbox/musicbox.ino
+++ musicbox/musicbox.ino
@@ -14,14 +14,14 @@ int notes[] = {NOTE_G1, NOTE_G4, NOTE_G1, NOTE_G4};
 double durations[] = {2,2, 2, 2};
 int melodyLength = 4;
 
-// maybe somewhere in the future we will have one more button...
-// #define PIN_BUTTON_MEL_TWO 4
-// Button buttonMelodyTwo(PIN_BUTTON_MEL_TWO);
+ maybe somewhere in the future we will have one more button...
+ #define PIN_BUTTON_MEL_TWO 4
+ Button buttonMelodyTwo(PIN_BUTTON_MEL_TWO);
 
-// and the second melody
-// int notes2[] = {NOTE_C4, NOTE_SILENCE, NOTE_G4, NOTE_SILENCE};
-// double durations2[] = {4, 1, 4, 1};
-// int melodyLength2 = 4;
+ and the second melody
+ int notes2[] = {NOTE_C4, NOTE_SILENCE, NOTE_G4, NOTE_SILENCE};
+ double durations2[] = {4, 1, 4, 1};
+ int melodyLength2 = 4;
 
 void setup()
 {
